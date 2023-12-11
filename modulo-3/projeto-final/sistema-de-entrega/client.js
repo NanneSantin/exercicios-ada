29f26fa04b0dc81ec4e9659c92d4731dceef9ff5
NanneSantin <elaine.s.santin@gmail.com>
2023-12-11 20:20:15 -0300

    refactor: Correção de bugs.

--- modulo-3/projeto-final/sistema-de-entrega/client.js
+++ modulo-3/projeto-final/sistema-de-entrega/client.js
@@ -1,24 +1,44 @@
+const Order = require("./order");
 class Client {
     #name;
     #email;
     #password;
     #address;
+    #orders;
 
     constructor(name, email, password, address) {
         this.#name = name;
         this.#email = email;
         this.#password = password;
         this.#address = address;
+        this.#orders = [];
     }
 
-    getClient() {
+    getCustomer() {
         return {
             name: this.#name,
             email: this.#email,
             password: this.#password,
             address: this.#address
         };
+    }
 
+    getInfosClient() {
+        return {
+            name: this.#name,
+            email: this.#email,
+            address: this.#address
+        };
+    }
+
+    makeOrder(restaurant, order) {
+        const newOrder = new Order(this.getInfosClient(), restaurant, order);
+
+        this.#orders.push(newOrder);
+    }
+
+    checkOrders() {
+        return this.#orders;
     }
 }
 
