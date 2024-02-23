cdbc6737c6faaaffc25ad2dfa731c950f194e2d4
NanneSantin <elaine.s.santin@gmail.com>
2024-02-22 22:01:48 -0300

    feat: adicionado a database.

---
+++ modulo-5/projeto-react/projeto-react/database/jogos.js
@@ -0,0 +1,34 @@
+const jogos = [
+    {
+        titulo: 'Minecraft',
+        linkImg:
+            'https://image.api.playstation.com/vulcan/img/cfn/11307uYG0CXzRuA9aryByTHYrQLFz-HVQ3VVl7aAysxK15HMpqjkAIcC_R5vdfZt52hAXQNHoYhSuoSq_46_MT_tDBcLu49I.png',
+    },
+    {
+        titulo: 'Super Mario Odyssey',
+        linkImg:
+            'https://assets.nintendo.com/image/upload/c_fill,w_1200/q_auto:best/f_auto/dpr_2.0/ncom/software/switch/70010000001130/c42553b4fd0312c31e70ec7468c6c9bccd739f340152925b9600631f2d29f8b5',
+    },
+    {
+        titulo: 'The Legend of Zelda: Breath of the Wild',
+        linkImg:
+            'https://assets.nintendo.com/image/upload/c_fill,w_1200/q_auto:best/f_auto/dpr_2.0/ncom/software/switch/70010000000025/7137262b5a64d921e193653f8aa0b722925abc5680380ca0e18a5cfd91697f58',
+    },
+    {
+        titulo: 'The Last Of Us',
+        linkImg:
+            'https://image.api.playstation.com/vulcan/ap/rnd/202206/0720/eEczyEMDd2BLa3dtkGJVE9Id.png',
+    },
+    {
+        titulo: 'FIFA 22',
+        linkImg:
+            'https://s2.glbimg.com/FsLKBxrdyVH7oZlCOGZQU63bJoc=/600x0/filters:quality(70)/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/K/N/j25djFT9izbgpARwS7Gw/fifa-22.jpeg',
+    },
+    {
+        titulo: 'Grand Theft Auto V',
+        linkImg:
+            'https://cdn2.unrealengine.com/Diesel%2Fproductv2%2Fgrand-theft-auto-v%2Fhome%2FGTAV_EGS_Artwork_1920x1080_Hero-Carousel_V06-1920x1080-1503e4b1320d5652dd4f57466c8bcb79424b3fc0.jpg',
+    },
+];
+
+export default jogos;
